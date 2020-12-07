8523e703a2aa04288e1fcfc33fb98631236a3a67
Lucas Pompeu <lucaspompeuneves@gmail.com>
2020-12-06 21:59:16 -0300

    Aula caracteres e textos

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando prjeto 5 - Caraceteres e textos");
+
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)61;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura cursos de tecnologia " + 2020;
+            string cursosProgramacao = @" - .NET
+ - Java
+ - JavaScritp";
+
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+
+            Console.ReadLine();
+        }
+    }
+}
