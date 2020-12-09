2c8730747825fdb0dcd4db3317c2048934614aad
Lucas Pompeu <lucaspompeuneves@gmail.com>
2020-12-08 21:56:32 -0300

    Aulas 4 e 5

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 7 - condicionais");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+
+            if (idadeJoao >= 18)
+            {
+                Console.WriteLine("Joao possui mais de 18 anos");
+            }
+            else
+            {
+                if (quantidadePessoas >= 1)
+                {
+                    Console.WriteLine("Menor de 18 e acompanhado");
+                }
+                else
+                {
+                    Console.WriteLine("Joao nao possui mais de 18 anos");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 8 - Condicionais 2");
+
+            int idadeJoao = 18;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "Joao esta acompanhado";
+            }
+            else
+            {
+                 mensagemAdicional = "Joao nao esta acompanhado";
+            }
+
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Nao pode entrar");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
