85b08c99c712bb1c5e635a2a65e315af13585de6
Lucas Pompeu <lucaspompeuneves@gmail.com>
2020-12-11 16:29:17 -0300

    Aula 6 - Parte 1

---
+++ P10-CalculaPoupanca/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando porojeto 10 - Calcula poupanca");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                valorInvestido = valorInvestido + (valorInvestido * 0.0036);
+                Console.WriteLine($"Após {contadorMes} meses, você terá " + valorInvestido);
+
+                contadorMes++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
