7f70f658eac14c704d60fa90e99f7d5c11aaf2e5
Lucas Pompeu <lucaspompeuneves@gmail.com>
2020-12-16 17:41:32 -0300

    Curso finalizado

---
+++ P13-ForEncadeado/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace P13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 13");
+
+            // *
+            // **
+            // ***
+            // ****
+
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+
+                    if (contadorColuna >= contadorLinha)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
